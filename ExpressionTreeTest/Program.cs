1fac98e31ed93f4235f59d1103f586a9169bde0b
mikekozlov <mykhail.kozlov@gmail.com>
2014-02-10 19:31:14 +0200

    initial commit
    initial test commit

---
+++ ExpressionTreeTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTreeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
